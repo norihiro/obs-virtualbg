cbe4fe9bce2e1cb500c26719162ac02d84d8d9d1
Norihiro Kamae <norihiro@nagater.net>
2022-04-23 21:08:06 +0900

    Sign and notarize for macOS

---
+++ scripts/notarize_mac.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+CODESIGN_IDENT_SHORT=$(echo "${MACOS_SIGNING_APPLICATION_IDENTITY}" | /usr/bin/sed -En "s/.+\((.+)\)/\1/p")
+
+pkgs=(build/obs-virtualbg-*.zip)
+uuids=()
+for pkg in "${pkgs[@]}"; do
+  echo "=> Submitting package $pkg for notarization"
+  UPLOAD_RESULT=$(xcrun altool \
+    --notarize-app \
+    --primary-bundle-id "$MACOS_BUNDLEID" \
+    --username "$MACOS_NOTARIZATION_USERNAME" \
+    --password "$MACOS_NOTARIZATION_PASSWORD" \
+    --asc-provider "$CODESIGN_IDENT_SHORT" \
+    --file "$pkg")
+  uuid=$(echo $UPLOAD_RESULT | awk -F ' = ' '/RequestUUID/ {print $2}')
+  echo "Request UUID: $uuid"
+  uuids=("${uuids[@]}" $uuid)
+  echo "$uuid $pkg" >> pkg_uuid
+done
+
+for uuid in "${uuids[@]}"; do
+  pkg="$(grep "$uuid" < pkg_uuid | cut -d\  -f2-)"
+  echo "Checking notarization status for package '$pkg' UUID=$uuid..."
+  while sleep 10; do
+    CHECK_RESULT=$(xcrun altool \
+      --notarization-info "$uuid" \
+      --username "$MACOS_NOTARIZATION_USERNAME" \
+      --password "$MACOS_NOTARIZATION_PASSWORD" \
+      --asc-provider "$CODESIGN_IDENT_SHORT")
+    echo $CHECK_RESULT
+
+    if ! grep -q "Status: in progress" <<< "$CHECK_RESULT"; then
+      if expr "$pkg" : '.*dmg$'; then
+        echo "=> Staple ticket to installer: $pkg"
+        xcrun stapler staple "$pkg"
+      fi
+      break
+    fi
+  done
+done
